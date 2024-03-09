1922d3c576edf4923830b3f4be18e47675e418db
Ramita <104021517+ramitaarora@users.noreply.github.com>
2024-03-08 21:54:12 -0800

    homework 2

---
+++ HW_2.java
@@ -0,0 +1,15 @@
+public class HW_2 {
+    int[] temperatures = new int[] {4,5,6,7,8};
+
+    public static void main(String[] args) {
+        Solution obj = new Solution();
+    }
+    
+}
+
+class Solution {
+
+    //public int[] getIndices(int[] nums, int target) {
+        // YOUR CODE HERE
+    //}
+}
