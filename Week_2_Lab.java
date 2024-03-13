c594a444fcc0c14a207dc90f0ab91de80179cc7a
Ramita <104021517+ramitaarora@users.noreply.github.com>
2024-03-13 09:41:34 -0700

    lab assignment

--- Week_2_Lab.java
+++ Week_2_Lab.java
@@ -1,8 +1,12 @@
+import java.util.Arrays;
+
 public class Week_2_Lab {
-    int[] temperatures = new int[] {4,5,6,7,8};
 
     public static void main(String[] args) {
+        int[] temperatures = new int[] {4,5,6,7,8};
         Solution obj = new Solution();
+        int[] output = obj.getIndices(temperatures, 14);
+        System.out.println(Arrays.toString(output));
     }
     
 }
@@ -12,6 +16,19 @@ class Solution {
     public Solution() {}
 
     public int[] getIndices(int[] nums, int target) {
-        return null;
+        int num1 = 0;
+        int num2 = 0;
+        
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length - 1; j++) {
+                if (nums[i] + nums[j] == target && nums[i] != nums[j]) {
+                    num1 = nums[i];
+                    num2 = nums[j];
+                    return new int[] {num1, num2};
+                }
+            }
+        }
+        return new int[0];
     }
 }
